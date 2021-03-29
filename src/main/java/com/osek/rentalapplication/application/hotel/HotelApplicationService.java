8e38aea4d15682cf9c34557417346236c4e8f992
Oskar Sęk <oskar.sek@3s.pl>
2021-03-29 22:16:04 +0200

    Add Hotel Factory

--- src/main/java/com/osek/rentalapplication/application/hotel/HotelApplicationService.java
+++ src/main/java/com/osek/rentalapplication/application/hotel/HotelApplicationService.java
@@ -7,5 +7,4 @@ public class HotelApplicationService {
     public void add(String name, String street, String postalCode, String buildingNumber, String city, String country) {
         Hotel hotel = new HotelFactory().create(name, street, postalCode, buildingNumber, city, country);
     }
-
 }
