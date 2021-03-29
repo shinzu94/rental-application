57229aad8dc9c965507a768b80a6945a172b27db
Oskar Sęk <oskar.sek@3s.pl>
2021-03-29 23:32:28 +0200

    new HotelRepository

--- src/main/java/com/osek/rentalapplication/application/hotelroom/HotelRoomApplicationService.java
+++ src/main/java/com/osek/rentalapplication/application/hotelroom/HotelRoomApplicationService.java
@@ -1,12 +1,20 @@
 package com.osek.rentalapplication.application.hotelroom;
 
+import com.osek.rentalapplication.domain.hotelroom.HotelRepository;
 import com.osek.rentalapplication.domain.hotelroom.HotelRoom;
 import com.osek.rentalapplication.domain.hotelroom.HotelRoomFactory;
 
 import java.util.Map;
 
 public class HotelRoomApplicationService {
+    private final HotelRepository hotelRepository;
+
+    public HotelRoomApplicationService(HotelRepository hotelRepository) {
+        this.hotelRepository = hotelRepository;
+    }
+
     public void add(String hotelId, int number, Map<String, Double> spacesDefinition, String description){
         HotelRoom hotelRoom = new HotelRoomFactory().create(hotelId, number, spacesDefinition, description);
+        hotelRepository.save(hotelRoom);
     }
 }
---
+++ src/main/java/com/osek/rentalapplication/domain/hotelroom/HotelRepository.java
@@ -0,0 +1,5 @@
+package com.osek.rentalapplication.domain.hotelroom;
+
+public interface HotelRepository {
+    void save(HotelRoom hotelRoom);
+}
