c026097a1b92d7638c3a43f968e8f5e1050105de
Oskar Sęk <oskar.sek@3s.pl>
2021-05-02 19:43:13 +0200

    change EventChannel to interface

--- src/main/java/com/osek/rentalapplication/domain/eventchanel/EventChannel.java
+++ src/main/java/com/osek/rentalapplication/domain/eventchanel/EventChannel.java
@@ -3,10 +3,8 @@ package com.osek.rentalapplication.domain.eventchanel;
 import com.osek.rentalapplication.domain.apartment.ApartmentBooked;
 import com.osek.rentalapplication.domain.hotelroom.HotelRoomBooked;
 
-public class EventChannel {
-    public void publish(ApartmentBooked apartmentBooked) {
-    }
+public interface EventChannel {
+    void publish(ApartmentBooked apartmentBooked);
 
-    public void publish(HotelRoomBooked hotelRoomBooked) {
-    }
+    void publish(HotelRoomBooked hotelRoomBooked);
 }
